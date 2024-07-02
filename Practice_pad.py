f6d9e0a76cba9a63f646696631e64faaa21a8ee9
wonjun <max12max@g.yju.ac.kr>
2024-07-02 20:11:44 +0900

    7/2

--- Practice_pad.py
+++ Practice_pad.py
@@ -1,30 +1,37 @@
 # 야구 게임 만들기
 import random
 # 컴퓨터 난수 생성 - 0 ~ 9 사이의 중복되지 않은 수 3개
+# 함수 설정 
 def get_random_list(rand_value, start_value, end_value):
-    
+    # 빈 리스트 생성
     random_list = []
-    
+    # 리스트안에 3개의 난수가 들어가면 반복을 멈춰야하니 카운트를 해줌
     random_count = 0
     
-    
     while random_count < rand_value:
-        
+        # 랜덤 난수 생성    
         random_num = random.randint(start_value, end_value)
-        
+        # 플레그 변수 
         found_value = False 
-        
+        # 인덱스 번호를 카운트 범위 만큼 반복
         for index in range(random_count):
+            # 리스트 안의 인덱스에 해당하는 숫자가 이번에 랜덤으로 뽑은 숫자와 같으면
             if random_list[index] == random_num:
+                # 플레그 변수 변경
                 found_value = True
+                # 정지
                 break
+            # 만약 플레그 변수가 변하지 않을 때
         if not found_value:
+            # 리스트에 랜덤 숫자 추가
             random_list.append(random_num)
+            # 카운트 올림
             random_count += 1
-            
+    # 리스트 값 반환
     return random_list
-
+# 함수 호출. 참조 변수 지정
 com_random_list = get_random_list(3, 0, 9)
+# 뽑은 랜덤 값 리스트 출력
 print("컴퓨터 랜덤 값: ", com_random_list)
 # 플레이어 입력 - 0 ~ 9 사이의 수
 count_game = 0
@@ -33,34 +40,51 @@ count_strike_out = 0
 while True:
     count_strike = 0
     count_ball = 0
-    
+    # 반복 할 때마다 시도 횟수 증가해야함
     count_game += 1
     print("사용자 입력: ")
+    # 사용자 입력을 바로 리스트에 넣음 3번 반복해서
     user_input = [int(input()) for _ in range(3)]
-    
+    # 리스트 내에서의 반복
     for i in range(3):
+        # 리스트 내에서의 반복
         for j in range(3):
+            # 만약 랜덤 리스트의 i번째가 사용자가 입력 리스트의 j번째와 같다면
             if com_random_list[i] == user_input[j]:
+                # 랜덤 리스트의 자리와 사용자 입력 리스트의 자리가 같으면
                 if i == j:
+                    # 스트라이크임
                     count_strike += 1
                 else:
+                    # 아니면 볼
                     count_ball += 1
                 break
+    # 만약 스트라이크랑 볼이 없으면
     if count_strike == 0 and count_ball == 0:
+        # 스트라이크 아웃 1개 올라감
         count_strike_out += 1
         print("스트라이크 아웃")
     else:
+        # 아니면 스트라이크와 볼의  횟수를 출력
         print("strike : ", count_strike, "ball : ", count_ball)
     
     
 
 # 게임 패배 조건 - 스트라이크 아웃 2회 이상, 시도 횟수 5번 이상
+    # 만약 스트라이크 아웃이 2번이상이면
     if count_strike_out >= 2:
+        # 패배 조건 달성
         print("종료 스트라이크 아웃 2회 이상")
+    # 만약 시도 횟수가 5번 이상이면 
     elif count_game >= 5:
+        # 패배 조건 달성
         print("종료 시도 횟수 5회 이상")
+        # 종료
         break
 # 게임 승리 조건 - 모든 숫자와 자리 까지 맞출경우
+    # 만약 스트라이크가 3번 이상이면
     if count_strike >= 3:
+        # 승리 조건 달성
        print("승리 게임 종료")
+        # 종료
        break
--- Practice_pad2.py
+++ Practice_pad2.py
@@ -2,53 +2,73 @@
 # 50% 블라인드 처리 하고 사용자가 입력하는 문자가 포함 되면 블라인드 해제
 # 해제할때 몇개 해제했는지 출력, 없으면 없다고 출력
 import random
-# 단어 입력 파트
+# 3개의 단어 입력을 위한 count
 count = ["첫", "두", "세"]
+# 빈 리스트 생성 - 사용자가 입력한 단어를 저장
 user_list = []
-# 단어 3개 입력
+# 카운트 리스트의 i번째 - "첫" 부터 시작
 for i in count:
     while True:
+        # 사용자 입력
         user1 = input(f"{i}번째 단어를 입력하세요")
-        # 단어 조건
+        # 만약 사용자가 입력한 단어의 길이가 3이상 20이하일 때
         if 3 <= len(user1) <= 20:
+            # 리스트에 추가
             user_list.append(user1)
+            break
         else:
+            # 아니면 추가 안하고 다시 입력
             print("재입력")
-# 입력한 단어중 랜덤으로 하나 선택
+# 입력한 3개의 단어중 랜덤으로 고름
 user_choice = random.choice(user_list)
-# 선택한 단어 리스트화
+# 위에서 랜덤으로 고른 단어를 한 문자씩 리스트에 저장
 user_choice_list = [value for value in user_choice]
+# 위의 리스트를 하나더 복사
 s_list = user_choice_list[:]
-
+# 50% 블라인드 해야하니 리스트의 길이를 구함
 count_word = len(user_choice_list)
+# 구한 리스트를 반으로 나눔
 half_word = count_word // 2
-# 리스트 길이가 짝수가 아니면 반으로 나눈거에 1 더해줘서 50%이상 블라인드 조건에 충족 시킴
+# 만약 구한 길이가 짝수가 아니면 
 if count_word % 2 != 0:
+    # 블라인드 할 단어 수에 1개 추가
     half_word += 1
-# 새로운 카운트 생성
+# 블라인드 했는지 확인용 카운트 추가
 count = 0
-rest_list = [] # 블라인드 처리할 인덱스 번호 저장
-while count < half_word: # count가 half-word보다 작을 때 까지만 반복
-    random_num = random.randint(0, half_word) # 범위는 0부터 half_word까지
-    if s_list[random_num] != "_": # 만약 랜덤으로 뽑은 인덱스의 인덱스가 "_"가 아니라면
-        s_list[random_num] = "_" # "_"로 변경
-        rest_list.append(random_num) # 그 변경했다면 그 인덱스 rest_list에 저장
-        count += 1 # 카운트올림
-        
-count = 0 # 시도 횟수 세는 카운트
+# 블라인드한 위치 인덱스 저장
+rest_list = []
+# 반복문 
+while count < half_word:
+    # 0부터 블라인드 할 
+    random_num = random.randint(0, count_word - 1)
+    # 만약 문자 리스트의 인덱스 번호가 "_"가 아니라면
+    if s_list[random_num] != "_":
+        # "_"로 변환
+        s_list[random_num] = "_"
+        # 바꾼 문자의 인덱스 번호를 빈리스트에 추가
+        rest_list.append(random_num)
+        # 카운트 증가
+        count += 1
+# 시도 횟수 카운트
+count = 0
 while True:
-    count_1 = 0 # 단어 내 입력한 문자가 몇개 해당하는지 확인용
+    # 단어에 몇개가 들어갔는지 확인용 카운트
+    count_1 = 0
+    # 시도 횟수 올라감
     count += 1
     print(f"단어 선택 완료 게임 시작 선택된 단어 : {user_choice}")
     print(f"{count}번째 시도, 아래 단어를 구성하는 문자를 입력하세요")
     print(s_list)
-    user_input = input() # 사용자 입력
-    for i in rest_list: # 블라인드된 단어의 인덱스 리스트중
-        if user_input == user_choice_list[i]: # 원래 단어리스트에 사용자가 입력한 단어가 들어있다면
-            s_list[i] = user_input # 그 인덱스를 입력한 단어로 바꿈
-            count_1 += 1 # 몇개 들어있는지 카운트
+    user_input = input()
+    # 블라인드 한 인덱스 위치중에서
+    for i in rest_list:
+        # 만약 사용자가 입력한 문자가 블라인드 처리한 문자와 같으면
+        if user_input == user_choice_list[i]:
+            # 
+            s_list[i] = user_input 
+            count_1 += 1 
     print(f"{s_list}, 입력한 문자 단어 내 포함 글자: {count_1}개")
     
-    if "_" not in s_list: # 만약 "_"가 블라인드된 인덱스 리스트에 없다면 
+    if "_" not in s_list: 
         print(f"게임 종료 선택된 단어 {user_choice}, 총 시도 횟수 {count}")
-        break # 게임종료
+        break 
