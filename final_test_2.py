d314cebd64017d204e931776c0f7d05de5c2e2b7
wonjun <max12max@g.yjp.ac.kr>
2024-07-02 20:16:16 +0900

    7/2 re

--- final_test_2.py
+++ final_test_2.py
@@ -10,12 +10,12 @@ def print_bingo_board(arg_list, col_num = 3):
 
 # N 값 입력
 while True:
-    N = int(input("N값: 3~6"))
+    N = int(input("N 값: 3 ~ 6"))
     
     if 3 <= N <= 6:
         break
     
-    bingo_element_num = N * N
+bingo_element_num = N * N
 # N X N 빙고 보드 작성: 1차원 배열, 1~36사이의 중복되지 않은 정수
 
 
@@ -27,28 +27,22 @@ while len(bingo_board) < bingo_element_num:
     
     if rand_num not in bingo_board:
         bingo_board.append(rand_num)
-
-print_bingo_board(bingo_board)
+        
+        
 
 bingo_count = 0
 
-# 대각선(오른쪽 -> 왼쪽) 확인 알고리즘
-for count in range(N):
-    if bingo_board[(count - 1) * (N - 1)] != "*": # count + (N * 2 - count)과 같은 소리이다.
-        break
-else:
-    bingo_count += 1
-print(bingo_count)
-
-# 빙고 숫자가 2미만일 경우
+# # 빙고 숫자가 2미만일 경우
 while bingo_count < 2:
     bingo_count = 0
     
-    input("랜덤 넘버 생성: Press enter")
+    
+    input("랜덤 넘버 생성: Press enter key!!")
+    
     # 랜덤 넘버 생성 : 1 ~ 36
     rand_num = random.randint(1, 36)
     
-    # 빙고 보드 내 생성된 랜덤 값이 있을 경우 "*"로 변경
+    #빙고 보드 내 생성된 랜덤 값이 있을 경우 "*"로 변경
     for index in range(bingo_element_num):
         if bingo_board[index] == rand_num:
             bingo_board[index] = "*"
@@ -56,37 +50,34 @@ while bingo_count < 2:
 
     # 빙고 보드 출력
     print_bingo_board(bingo_board)
+
     # 빙고 검사
     # 가로 확인 알고리즘
     for row in range(N):
         for col in range(N):
-            if bingo_board[col + (row * N)] != "*": # 가로줄 넘어가야해서 row에 N을 곱해서 밑에줄 인덱스로 변경함
+            if bingo_board[col + (row * N)] != "*":
                 break
         else:
             bingo_count += 1
-    print(bingo_count)
-
+    
     # 세로 확인 알고리즘
     for col in range(N):
         for row in range(N):
-            if bingo_board[col + (row * N)] != "*": # 세로줄로 넘어가기 위해 col에 row랑N을 곱한 값을 더해줌
+            if bingo_board[col + (row * N)] != "*":
                 break
         else:
             bingo_count += 1
-    print(bingo_count)
 
-    # 대각선(왼쪽 -> 오른쪽) 확인 알고리즘
+    # 대각선 : 왼쪽 -> 오른쪽 \
     for count in range(N):
-        if bingo_board[count * (N + 1)] != "*":
+        if bingo_board[ count * (N + 1) ] != "*":
             break
     else:
         bingo_count += 1
-    print(bingo_count)
 
-    # 대각선(오른쪽 -> 왼쪽) 확인 알고리즘
+    # 대각선 : 오른쪽 -> 왼쪽 /
     for count in range(N):
-        if bingo_board[(count - 1) * (N - 1)] != "*": # count + (N * 2 - count)과 같은 소리이다.
+        if bingo_board[(count + 1) * (N - 1)] != "*":
             break
     else:
         bingo_count += 1
-    print(bingo_count)
