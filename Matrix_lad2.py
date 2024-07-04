73ca8695f7275f1ca2264b93b65c3963e0396e28
wonjun <max12max@g.yjp.ac.kr>
2024-07-04 19:53:15 +0900

    7/4

---
+++ Matrix_lad2.py
@@ -0,0 +1,10 @@
+# 2차원 리스트 조작
+
+user_row = int(input("Enter the number of rows : "))
+user_col = int(input("Enter the number of columns : "))
+for _ in range():
+    user_choice = int(input(f"{[][]} : "))
+    
+
+bar = [[user_choice for _ in range(user_col)] for _ in range(user_row)]
+print(bar)
